06f92f60425a778a20223a8726fe256a4a1f2c2e
aishwarya_g <aishu047@gmail.com>
2019-01-22 23:29:56 +0530

    Initial commit

--- src/main/java/com/stackroute/controller/MuzixController.java
+++ src/main/java/com/stackroute/controller/MuzixController.java
@@ -1,6 +1,8 @@
 package com.stackroute.controller;
 
 import com.stackroute.domain.Muzix;
+import com.stackroute.exception.MuzixAlreadyExistsException;
+import com.stackroute.exception.MuzixNotFoundException;
 import com.stackroute.service.MuzixService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,16 +22,27 @@ public class MuzixController {
     @PostMapping("muzix")
     public ResponseEntity<?> saveMuzix(@RequestBody Muzix muzix){
         ResponseEntity responseEntity;
-        muzixService.saveMuzix(muzix);
-        responseEntity = new ResponseEntity<String>("Succesfully inserted", HttpStatus.CREATED);
-        return responseEntity;
+        try {
+            muzixService.saveMuzix(muzix);
+            responseEntity = new ResponseEntity<String>("Succesfully inserted", HttpStatus.CREATED);
+        }catch(MuzixAlreadyExistsException e){
+            responseEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
+
+        }
+            return responseEntity;
     }
     //to delete
     @DeleteMapping("muzix/{id}")
     public ResponseEntity<?> deleteMuzix(@PathVariable int id){
-        muzixService.deleteById(id);
-        ResponseEntity responseEntity = new ResponseEntity<List<Muzix>>(muzixService.getAllMuzix(),HttpStatus.OK);
-        return responseEntity;
+        ResponseEntity responseEntity;
+        try {
+            muzixService.deleteById(id);
+            responseEntity = new ResponseEntity<List<Muzix>>(muzixService.getAllMuzix(), HttpStatus.OK);
+        }catch (MuzixNotFoundException e){
+            responseEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
+
+        }
+            return responseEntity;
     }
     //fetch(GET) data
     @GetMapping("muzix")
@@ -39,12 +52,29 @@ public class MuzixController {
     }
     //track(get) by id
     @GetMapping("muzix/{id}")
-    public ResponseEntity<?> getTrack(@PathVariable int id){
-        return new ResponseEntity<Muzix>(muzixService.getTrack(id),HttpStatus.OK);
+    public ResponseEntity<?> getTrack(@PathVariable int id) {
+        ResponseEntity responseEntity;
+        try {
+            responseEntity =  new ResponseEntity<Muzix>(muzixService.getTrack(id), HttpStatus.OK);
+        } catch (MuzixNotFoundException e) {
+            responseEntity = new ResponseEntity<String>(e.getMessage(),HttpStatus.CONFLICT);
+        }
+        return responseEntity;
     }
     //to update
     @PutMapping("muzix")
     public ResponseEntity<?> updateMuzix(@RequestBody Muzix muzix){
       return new ResponseEntity<Muzix>(muzixService.updateTrackComment(muzix),HttpStatus.OK);
+    }
+
+    @GetMapping("muzix/{trackName}")
+    public ResponseEntity<?> getTrackByName(@PathVariable String trackName){
+        ResponseEntity responseEntity;
+        try{
+            responseEntity = new ResponseEntity<Muzix>(muzixService.getTrackByName(trackName),HttpStatus.OK);
+        }catch (MuzixNotFoundException e){
+            responseEntity = new ResponseEntity<String>(e.getMessage(),HttpStatus.CONFLICT);
+        }
+        return responseEntity;
     }
 }
